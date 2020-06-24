897a8e0eafbb6a99639b8a4185b30cbab28ff0b1
zhangwei <514970358@qq.com>
2020-06-24 14:44:10 +0800

    暂存

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './index.scss'
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="P-home">
+                <h1>Home page</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
---
+++ src/pages/Login/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './index.scss'
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="P-login">
+                <h1>Login page</h1>
+            </div>
+        )
+    }
+}
+
+export default Login
