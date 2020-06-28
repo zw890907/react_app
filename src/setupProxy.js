6d45e341e87c8674ea0e7e43ada60f7706e00abb
zhangwei <514970358@qq.com>
2020-06-28 15:40:13 +0800

    暂存

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,13 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
+    
     app.use(
         '/v2',
         createProxyMiddleware({
             target: 'http://spidercms.wjdev.chinamcloud.cn/',
             // target: 'http://cmsfront.wjtest.chinamcloud.cn/v2',
+            // target: 'http://172.29.3.231:3000/',
             changeOrigin: true,
             pathRewrite: {
                 '^/v2': ''
